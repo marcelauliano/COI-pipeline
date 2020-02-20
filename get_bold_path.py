c32deae989d199929c52b9271839cd282e0949ac
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2020-02-20 16:17:52 +0000

    Update get_bold_path.py

--- get_bold_path.py
+++ get_bold_path.py
@@ -1,4 +1,8 @@
 import sys
+
+#Giving a input wich is a text file containing a species name in each line (genus species), print a line that can be easily used to make a bold download of the COI-5P sequences
+
+
 input = open(sys.argv[1]).readlines() 
 output=open(sys.argv[2], "w")
 for f in input:
