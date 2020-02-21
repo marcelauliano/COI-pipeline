84f346d53ba8cf85d4725650ee9129bcf2958db4
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2020-02-21 17:05:32 +0000

    Update get_bold_path.py

--- get_bold_path.py
+++ get_bold_path.py
@@ -6,8 +6,9 @@ import sys
 input = open(sys.argv[1]).readlines() 
 output=open(sys.argv[2], "w")
 for f in input:
-  split=f.rstrip('\n').split(" ")
-   genus=split[0]
-   species=split[1]
+  (genus, species)=f.rstrip('\n').split(" ")
+  if (species):
    output.write("wget http://v3.boldsystems.org/index.php/API_Public/sequence?taxon=" + genus + "%20" + species + "&marker=COI-5P" + "\n")
 output.close()
+else:
+  
