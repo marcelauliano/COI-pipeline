3eefb2a00cd0d2507bcca50fa3ee5f689b47e488
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2020-02-24 13:31:01 +0000

    Update get_bold_path.py

--- get_bold_path.py
+++ get_bold_path.py
@@ -2,13 +2,17 @@ import sys
 
 #Giving a input wich is a text file containing a species name in each line (genus species), print a line that can be easily used to make a bold download of the COI-5P sequences
 
-
 input = open(sys.argv[1]).readlines() 
 output=open(sys.argv[2], "w")
 for f in input:
-  (genus, species)=f.rstrip('\n').split(" ")
-  if (species):
-   output.write("wget http://v3.boldsystems.org/index.php/API_Public/sequence?taxon=" + genus + "%20" + species + "&marker=COI-5P" + "\n")
+    split=f.rstrip('\n').split(" ")
+    if len(split) !=2:
+        continue
+    genus=split[0]
+    species=split[1]
+    if species: 
+        output.write("wget http://v3.boldsystems.org/index.php/API_Public/sequence?taxon=" + genus + "%20" + species + "&marker=COI-5P" + "\n")
+    else:
+        output.write("wget http://v3.boldsystems.org/index.php/API_Public/sequence?taxon=" + genus + "&marker=COI-5P" + "\n")
 output.close()
-else:
   
