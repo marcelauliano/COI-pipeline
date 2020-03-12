b0dc90561aa782b57fcd5c259f119dc0e8d89e01
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2020-03-12 10:02:06 +0000

    Update coi_folder_find.py

--- coi_folder_find.py
+++ coi_folder_find.py
@@ -7,22 +7,26 @@ today= date.today()
 parser= argparse.ArgumentParser(add_help=False)
 parser.add_argument("-h", "--help", action="help", default=argparse.SUPPRESS, help= "This script was written to deal with the Sanger Tola assembly team folder structure. It searches for a folder called 'coi' from the path given as first argument '-p'. Arguments are described as follows") 
 parser.add_argument("-p", help= "-p is root path directory to look for 'coi' folder in subsequent directories", required = "True")
-parser.add_argument("-output1", help= "output1 is a file that presents a list of directories that contain a folder 'coi'", required = "True")
-parser.add_argument("-output2", help= "output1 is a file that presents a list of directories that DOES NOT contain a folder 'coi'", required = "True")
+parser.add_argument("-out1", help= "output1 is a file that presents a list of directories that contain a folder 'coi'", required = "True")
+parser.add_argument("-out2", help= "output1 is a file that presents a list of directories that DOES NOT contain a folder 'coi'", required = "True")
 args = parser.parse_args()
-output1=open(args.output1, "w")
-output2=open(args.output2, "w")
+output1=open(args.out1, "w")
+output2=open(args.out2, "w")
 path =[ ]
 path2 = [ ]
 for rootDir, dirs, files in os.walk(args.p):
     for d in dirs:
         if 'coi' in dirs:
             path.append(os.path.join(rootDir, d))
-            for i in path:
-                output1.write("As of" + " " + str(today) + " " + "we have a coi folder for" + " " + str(i) + "\n")
         else:
-            path2.append(os.path.join(rootDir, d))
-            for m in path2:
-                output2.write("As of" + " " + str(today) + " " + "we don't have a coi folder for" + " " + str(m) + "\n")
+            if 'PacBio' in dirs:
+                path2.append(os.path.join(rootDir, d))
+            
+
+for i in path:
+    output1.write("As of" + " " + str(today) + " " + "we have a coi folder for" + " " + str(i) + "\n")                
+for m in path2:
+    output2.write("As of" + " " + str(today) + " " + "we don't have a coi folder for" + " " + str(m) + "\n")  
+        
 output1.close()
 output2.close()
