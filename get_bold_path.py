e21ef20797c8674b3ef335da38730e84ea7d6e85
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2020-03-18 16:10:55 +0000

    Update get_bold_path.py

--- get_bold_path.py
+++ get_bold_path.py
@@ -7,19 +7,22 @@ import re
 
 input = open(sys.argv[1]).readlines() 
 output=open(sys.argv[2], "w")
+output2=open(sys.argv[3], "w")
 for f in input:
-    split=f.rstrip('\n').split(" ")
-    if len(split) !=2:
-        continue
-    genus=split[0]
-    species=split[1]
-    m = re.search(r'\d*', species) 
-    if m is not None:
-        output.write("wget" + genus + "%20"  + "&marker=COI-5P" + "\n")
-    elif m is None:
-            output.write("wget" + genus + "%20" + species + "&marker=COI-5P" + "\n")
-    else:
-        output.write("wget" + genus + "%20"  + "&marker=COI-5P" + "\n")
+        split=f.rstrip('\n').split(" ")
+        if len(split) !=2:
+                continue
+        genus=split[0]
+        species=split[1]
+        m = re.search(r'\d$', species) 
+        if m is not None:
+                output2.write(genus + "\n")
+                output.write("wget" + " " + genus + "%20"  + "&marker=COI-5P" + "\n")
+        elif m is None:
+                output2.write(genus + " " + species + "\n")
+                output.write("wget" + " " + genus + "%20" + species + "&marker=COI-5P" + "\n")
+        else:
+                output2.write(genus + "\n")
+                output.write("wget" + " " + genus + "%20"  + "&marker=COI-5P" + "\n")
 output.close()
-
   
