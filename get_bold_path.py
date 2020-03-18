c2be7efa4dfce5bd3390e1b7f5f5acec67758e3d
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2020-03-18 14:20:28 +0000

    Update get_bold_path.py

--- get_bold_path.py
+++ get_bold_path.py
@@ -1,6 +1,9 @@
 import sys
+import re
 
-#Giving a input wich is a text file containing a species name in each line (genus species), print a line that can be easily used to make a bold download of the COI-5P sequences
+#Giving a input wich is a text file containing a species name in each line (genus species), print a line that can be easily used to make a bold download of the COI-5P sequences. This script will also edit the input file, it will produce a bold line only for the genus, if the species variable contains numbers or is empty.
+
+#Usage: python get_bold_path.py <list.name.file> <bold_line>
 
 input = open(sys.argv[1]).readlines() 
 output=open(sys.argv[2], "w")
@@ -10,9 +13,13 @@ for f in input:
         continue
     genus=split[0]
     species=split[1]
-    if species: 
-        output.write("wget http://v3.boldsystems.org/index.php/API_Public/sequence?taxon=" + genus + "%20" + species + "&marker=COI-5P" + "\n")
+    m = re.search(r'\d*', species) 
+    if m is not None:
+        output.write("wget" + genus + "%20"  + "&marker=COI-5P" + "\n")
+    elif m is None:
+            output.write("wget" + genus + "%20" + species + "&marker=COI-5P" + "\n")
     else:
-        output.write("wget http://v3.boldsystems.org/index.php/API_Public/sequence?taxon=" + genus + "&marker=COI-5P" + "\n")
+        output.write("wget" + genus + "%20"  + "&marker=COI-5P" + "\n")
 output.close()
+
   
