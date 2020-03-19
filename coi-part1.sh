4028ddb716d063d113cc63de270c3b87dbf18a82
Marcela Uliano-Silva <marcela.uliano@gmail.com>
2020-03-19 13:54:50 +0000

    Update coi-part1.sh

--- coi-part1.sh
+++ coi-part1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 software="/software/team311/mu2/coi"
-new_species="ectemnius_lituratus" #replace this with species name. Ex: homo_sapiens" 
-concat="sequence?taxon=Ectemnius%20lituratus" #change here with the species name. Genus after taxon= and species after %20
-python $software/get_bold.py -s $new_species -o out
+new_species="ectemnius_lituratus" #replace this with species name. Ex: homo_sapiens
+python $software/get_bold.py -s $new_species -o out #python script that generates a wget line for coi-5p of the species present in the variable new_species 
+sh out #execute the wget line to download coi-5p sequences
 echo "all done"
 done
